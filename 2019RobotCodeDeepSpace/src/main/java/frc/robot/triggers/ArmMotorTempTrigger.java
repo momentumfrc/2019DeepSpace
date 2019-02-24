43fb2995f72d5764980778533c86b377a601f69f
Jordan Powers <jordanpowers1227@gmail.com>
2019-02-24 01:24:11 -0800

    Arm temperature cutoff
    Not sure if this was desired, but the code for it was half-there, so I just finished it

--- 2019RobotCodeDeepSpace/src/main/java/frc/robot/triggers/ArmMotorTempTrigger.java
+++ 2019RobotCodeDeepSpace/src/main/java/frc/robot/triggers/ArmMotorTempTrigger.java
@@ -2,19 +2,28 @@ package frc.robot.triggers;
 
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.buttons.Trigger;
-import frc.robot.Robot;
-import frc.robot.subsystems.Arm;
+import frc.robot.RobotMap;
 
 public class ArmMotorTempTrigger extends Trigger {
     private static final double TEMP_CUTOFF = 100;  // Degrees Celcius
     private static final double TIME_CUTOFF = 7500; // Time in ms that the cutoff will take to activate
 
-    private Arm arm = Robot.arm;
+    private long start_time;
+
+    private long getTimeMillis() {
+        return (long) (1000 * Timer.getFPGATimestamp());
+    }
 
     @Override
     public boolean get() {
-        return false;
+        if(RobotMap.armMotor.getMotorTemperature() > TEMP_CUTOFF) {
+            return getTimeMillis() - start_time > TIME_CUTOFF;
+        } else {
+            start_time = getTimeMillis();
+            return false;
+        }
     }
 
 }
