e2dfaffc9b6cb9e64c5e376b34f213acb71ddcb2
Terry <terbos90803@gmail.com>
2019-05-17 19:05:02 -0700

    Move Controls chooser to Shuffleboard

--- 2019RobotCodeDeepSpace/src/main/java/frc/robot/choosers/ControlChooser.java
+++ 2019RobotCodeDeepSpace/src/main/java/frc/robot/choosers/ControlChooser.java
@@ -1,9 +1,9 @@
 package frc.robot.choosers;
 
+import frc.robot.RobotMap;
 import frc.robot.controllers.*;
-
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ControlChooser extends SendableChooser<DriveController> {
 
@@ -14,7 +14,8 @@ public class ControlChooser extends SendableChooser<DriveController> {
     setDefaultOption("Xbox Drive/Arm, F310 Wrist/Intake", new XboxF310Wrapper());
     addOption("Xbox Drive, F310 Arm/Wrist/Intake", new XboxF310Wrapper2());
 
-    SmartDashboard.putData(NAME, this);
+    RobotMap.matchTab.add(NAME, this).withPosition(7, 0).withWidget(BuiltInWidgets.kSplitButtonChooser);
+
   }
 
 }
