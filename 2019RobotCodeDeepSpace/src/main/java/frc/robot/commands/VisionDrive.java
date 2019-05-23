f8d707383759ebb55d36cece62cf35dbd314c47d
Jacob Cook <jacob_a_cook@outlook.com>
2019-05-23 11:15:32 -0700

    Checked for validity of positions and made targetMet() a class member

--- 2019RobotCodeDeepSpace/src/main/java/frc/robot/commands/VisionDrive.java
+++ 2019RobotCodeDeepSpace/src/main/java/frc/robot/commands/VisionDrive.java
@@ -10,16 +10,15 @@ import frc.robot.utils.Utils;
 public class VisionDrive extends Command {
   private DriveSubsystem drive = Robot.driveSystem;
   private Limelight limelight = Robot.limelight;
-  private LimelightData data;
 
-  public static final double RANGE_X = 29.8; // The range of values given by "tx" is from -29.8 to 29.8 degrees
-  public static final double RANGE_Y = 24.85; // The range of values given by "ty" is from -24.85 to 24.85 degrees
+  public static final double RANGE_X = Limelight.RANGE_X; // The range of values given by "tx" is from -29.8 to 29.8
+                                                          // degrees
+  public static final double RANGE_Y = Limelight.RANGE_Y; // The range of values given by "ty" is from -24.85 to 24.85
+                                                          // degrees
 
-  public double turnErr;
-  public double posY;
-  public double dist;
   public double limelightTurn;
   public double limelightMove;
+  public boolean met;
 
   // TODO Tune these values
   public static final double TURN_K = .01;
@@ -36,19 +35,27 @@ public class VisionDrive extends Command {
   }
 
   protected void execute() {
-    limelight.getData();
-    turnErr = data.xCoord();
-    dist = data.dist();
+    LimelightData data = limelight.getData();
+    boolean valid = data.valid();
+    double turnErr = data.xCoord();
+    double dist = data.dist();
+    double turnRequest;
+    double moveRequest;
+    met = data.targetMet();
 
-    double turnRequest = Utils.map(turnErr, -RANGE_X, RANGE_X, -1.0, 1.0);
-    double moveRequest = Utils.map(dist, -RANGE_Y, RANGE_Y, -1.0, 1.0);
-
+    if (valid) {
+      turnRequest = Utils.map(turnErr, -RANGE_X, RANGE_X, -1.0, 1.0);
+      moveRequest = Utils.map(dist, -RANGE_Y, RANGE_Y, -1.0, 1.0);
+    } else {
+      turnRequest = 0;
+      moveRequest = 0;
+    }
     drive.arcadeDrive(moveRequest, turnRequest, MAX_DRIVE);
   }
 
   @Override
   protected boolean isFinished() {
-    return data.targetMet();
+    return met;
   }
 
   public void end() {
