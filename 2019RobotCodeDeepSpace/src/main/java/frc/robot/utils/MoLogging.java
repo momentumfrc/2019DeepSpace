89ce7d59c83c24276b65bacbbe0fc761ad383ae2
Jacob Cook <jacob_a_cook@outlook.com>
2019-07-12 00:06:56 -0700

    Start of Project
    Working on a way to log whatever data we want to from the RoboRIO onto a USB Flashdrive. 
    Goals:
    - Easily change what we want to log
    - Automatic FIle Naming and Sorting
    - Easy to implement onto other repos in the future

---
+++ 2019RobotCodeDeepSpace/src/main/java/frc/robot/utils/MoLogging.java
@@ -0,0 +1,14 @@
+package frc.robot.utils;
+
+/*
+ * This is intended as a program allowing the team to easily log match data to
+ * USB flash drive so that robot preformance can be tuned and monitored.
+ */
+
+import java.io.File;
+import java.util.logging.*;
+import edu.wpi.first.wpilibj.DriverStation;
+
+public class MoLogging {
+
+}
