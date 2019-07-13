cde998f498dd9df5f30705d55dcd8098f4ff76e5
Jacob Cook <jacob_a_cook@outlook.com>
2019-07-12 19:27:07 -0700

    File Structure
    Started the base work for the file structuring system so that the data logged will be stored and sorted in a logical easy to follow way

--- 2019RobotCodeDeepSpace/src/main/java/frc/robot/utils/MoLogging.java
+++ 2019RobotCodeDeepSpace/src/main/java/frc/robot/utils/MoLogging.java
@@ -11,4 +11,50 @@ import edu.wpi.first.wpilibj.DriverStation;
 
 public class MoLogging {
 
+  // Desired File path: eventName//match + Match Number//LOGS
+  public MoLogging() {
+    newMatch();
+  }
+
+  private void newMatch() {
+    String event = getMatchInfo().event();
+    int match = getMatchInfo().matchNumber();
+    String filenameMatch = Integer.toString(match);
+    File eventFolder = new File(event);
+    eventFolder.mkdir();
+    File matchFolder = new File(eventFolder, filenameMatch);
+    matchFolder.mkdir();
+  }
+
+  public MatchInfo getMatchInfo() {
+    return new MatchInfo(DriverStation.getInstance().getEventName(), DriverStation.getInstance().getMatchNumber(),
+        DriverStation.getInstance().getLocation());
+  }
+
+  public class MatchInfo {
+    private final int matchNumber;
+    private final int location;
+    private final String event;
+
+    private MatchInfo(String event, int matchNumber, int location) {
+      this.matchNumber = matchNumber;
+      this.location = location;
+      this.event = event;
+    }
+
+    // Returns the Match Number that is being recorded
+    public int matchNumber() {
+      return matchNumber;
+    }
+
+    // Returns the Location for the driver station during the match
+    public int location() {
+      return location;
+    }
+
+    // Returns the Event name
+    public String event() {
+      return event;
+    }
+  }
 }
