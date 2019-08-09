484b53523f081c9f49d94231d2c0eb0aa7d6fce3
Jordan Powers <jordanpowers1227@gmail.com>
2019-08-08 22:20:10 -0700

    Added Limelight Widget

--- 2019RobotCodeDeepSpace/src/main/java/frc/robot/subsystems/vision/Limelight.java
+++ 2019RobotCodeDeepSpace/src/main/java/frc/robot/subsystems/vision/Limelight.java
@@ -1,13 +1,22 @@
 package frc.robot.subsystems.vision;
 
+import java.util.Map;
+
+import edu.wpi.cscore.HttpCamera;
 import edu.wpi.first.networktables.*;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import frc.robot.RobotMap;
 
 public class Limelight extends Subsystem {
   private final NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
   private final NetworkTableEntry xCoordEntry = table.getEntry("tx");
   private final NetworkTableEntry yCoordEntry = table.getEntry("ty");
   private final NetworkTableEntry validEntry = table.getEntry("tv");
+
+  private NetworkTableEntry txWidget, tyWidget, validWidget, distanceWidget;
 
   private final NetworkTableEntry ledMode = table.getEntry("ledMode");
 
@@ -28,6 +37,24 @@ public class Limelight extends Subsystem {
   private static final double CAMERA_HEIGHT = 0;
   private static final double TARGET_HEIGHT = 0;
   // http://docs.limelightvision.io/en/latest/cs_estimating_distance.html
+
+  // CAMREA_ANGLE = Math.atan((TARGET_HEIGHT - CAMERA_HEIGHT) / distance ) -
+  // yCoordEntry.getNumber();
+
+  public Limelight() {
+    ShuffleboardLayout layout = RobotMap.testTab.getLayout("Limelight", BuiltInLayouts.kGrid).withSize(4, 4)
+        .withPosition(8, 0);
+    // This needs to be invesitgated. I'm not sure if the roborio is inefficiently
+    // re-streaming the limelight's stream
+    layout.add(new HttpCamera("Limelight", "10.49.99.11:5800", HttpCamera.HttpCameraKind.kMJPGStreamer))
+        .withPosition(0, 0).withSize(4, 3).withProperties(Map.of("Show controls", false));
+    txWidget = layout.add("tx", -1).withPosition(0, 3).withSize(1, 1).getEntry();
+    tyWidget = layout.add("ty", -1).withPosition(1, 3).withSize(1, 1).getEntry();
+    distanceWidget = layout.add("distance", -1).withPosition(2, 3).withSize(1, 1).getEntry();
+    validWidget = layout.add("valid", false).withWidget(BuiltInWidgets.kBooleanBox).withPosition(3, 3).withSize(1, 1)
+        .getEntry();
+
+  }
 
   public static enum LED_MODE {
     DEFAULT(0), OFF(1), BLINK(2), ON(3);
@@ -52,27 +79,39 @@ public class Limelight extends Subsystem {
 
   }
 
+  private double calculateDistance(double yCoord) {
+    double height = TARGET_HEIGHT - CAMERA_HEIGHT;
+    double angle = Math.tan(CAMERA_ANGLE + yCoord);
+    double distance = (height) / (angle);
+    return distance;
+  }
+
   public LimelightData getData() {
-    return new LimelightData(validEntry.getDouble(0), xCoordEntry.getDouble(0), yCoordEntry.getDouble(0));
+    double xCoord = validEntry.getDouble(0);
+    double yCoord = yCoordEntry.getDouble(0);
+    double distance = calculateDistance(yCoord);
+    boolean valid = validEntry.getDouble(0) == 1;
+
+    txWidget.setNumber(xCoord);
+    tyWidget.setNumber(yCoord);
+    distanceWidget.setNumber(distance);
+    validWidget.setBoolean(valid);
+
+    return new LimelightData(valid, xCoord, yCoord, distance);
   }
 
   public class LimelightData {
 
     private final double xCoord;
     private final double yCoord;
-    private final double valid;
     private final boolean validTarget;
     private final double dist;
 
-    private LimelightData(double valid, double xCoord, double yCoord) {
-      this.valid = valid;
+    private LimelightData(boolean valid, double xCoord, double yCoord, double dist) {
       this.xCoord = xCoord;
       this.yCoord = yCoord;
-      validTarget = valid == 1;
-
-      double height = TARGET_HEIGHT - CAMERA_HEIGHT;
-      double angle = Math.tan(CAMERA_ANGLE + yCoord);
-      dist = (height) / (angle);
+      this.validTarget = valid;
+      this.dist = dist;
     }
 
     public double dist() {
@@ -101,6 +140,11 @@ public class Limelight extends Subsystem {
         }
       }
       return false;
+    }
+
+    @Override
+    public String toString() {
+      return String.format("X:%.4f Y:%.4f dist:%.2f valid:%d", xCoord, yCoord, dist, validTarget ? 1 : 0);
     }
   }
 
