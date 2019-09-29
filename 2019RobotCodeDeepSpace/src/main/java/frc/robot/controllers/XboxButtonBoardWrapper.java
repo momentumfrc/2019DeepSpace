b44e1dc7a32375ec9b7c26e106e8d02869c6b217
Jacob Cook <jacob_a_cook@outlook.com>
2019-09-28 19:32:27 -0700

    Competition Fixes
    Assigned proper values to the all the buttons

--- 2019RobotCodeDeepSpace/src/main/java/frc/robot/controllers/XboxButtonBoardWrapper.java
+++ 2019RobotCodeDeepSpace/src/main/java/frc/robot/controllers/XboxButtonBoardWrapper.java
@@ -1,6 +1,5 @@
 package frc.robot.controllers;
 
-import edu.wpi.first.wpilibj.GenericHID.Hand;
 import static frc.robot.utils.Utils.*;
 
 /**
@@ -13,55 +12,49 @@ public class XboxButtonBoardWrapper extends ControllerBase {
 
   @Override
   public double getWristSpeed() {
-    double left_wrist = buttonBoard.getY(Hand.kLeft);
-    left_wrist = deadzone(left_wrist, DEADZONE);
-    left_wrist = curve(left_wrist, WRIST_CURVE);
-    double right_wrist = buttonBoard.getY(Hand.kRight);
-    right_wrist = deadzone(right_wrist, DEADZONE);
-    right_wrist = curve(right_wrist, WRIST_CURVE);
-    double wristspeed = left_wrist + right_wrist;
+    double wristspeed = buttonBoard.getRawAxis(1);
     wristspeed = clip(wristspeed, -1, 1);
     return wristspeed;
   }
 
   @Override
   public boolean getIntakeCargo() {
-    return buttonBoard.getButton(7);
+    return buttonBoard.getButton(12);
   }
 
   @Override
   public boolean getShootCargo() {
-    return buttonBoard.getButton(8);
+    return buttonBoard.getButton(11);
   }
 
   @Override
   public boolean getGrabHatch() {
-    return buttonBoard.getButton(9);
-  }
-
-  @Override
-  public boolean getKick() {
     return buttonBoard.getButton(10);
   }
 
   @Override
+  public boolean getKick() {
+    return buttonBoard.getButton(8);
+  }
+
+  @Override
   public boolean getSelectPresetHatchGround() {
-    return buttonBoard.getButton(0);
+    return buttonBoard.getButton(9);
   }
 
   @Override
   public boolean getSelectPresetHatch1() {
-    return buttonBoard.getButton(1);
+    return buttonBoard.getButton(6);
   }
 
   @Override
   public boolean getSelectPresetHatch2() {
-    return buttonBoard.getButton(2);
+    return buttonBoard.getButton(3);
   }
 
   @Override
   public boolean getSelectPresetCargoGround() {
-    return buttonBoard.getButton(3);
+    return buttonBoard.getButton(7);
   }
 
   @Override
@@ -71,12 +64,12 @@ public class XboxButtonBoardWrapper extends ControllerBase {
 
   @Override
   public boolean getSelectPresetCargo2() {
-    return buttonBoard.getButton(5);
+    return buttonBoard.getButton(1);
   }
 
   @Override
   public boolean getSelectPresetCargoBay() {
-    return buttonBoard.getButton(6);
+    return buttonBoard.getButton(5);
   }
 
 }
